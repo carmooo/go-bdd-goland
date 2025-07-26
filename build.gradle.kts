449e35a0317e2c78f55947e881b24d681836aa15
carmooo <joao.carmo@paddle.com>
2025-07-26 12:26:50 +0100

    chore: initial gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,64 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "2.1.0"
-  id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.7.0"
 }
 
 group = "com.github.carmooo"
 version = "1.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
-  intellijPlatform {
-    defaultRepositories()
-  }
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
-  intellijPlatform {
-    create("IC", "2025.1")
-    testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    intellijPlatform {
+        create("GO", "2024.3")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
-    // Add necessary plugin dependencies for compilation here, example:
-    // bundledPlugin("com.intellij.java")
-  }
+        plugin("gherkin", "243.21565.122")
+
+        pluginVerifier()
+        zipSigner()
+    }
 }
 
 intellijPlatform {
-  pluginConfiguration {
-    ideaVersion {
-      sinceBuild = "251"
-    }
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "243"
+        }
 
-    changeNotes = """
+        changeNotes = """
       Initial version
     """.trimIndent()
-  }
+    }
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "21"
-    targetCompatibility = "21"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "21"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+    }
+
+/*    publishPlugin {
+        token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+    }
+
+    signPlugin {
+        certificateChain.set(providers.environmentVariable("CERTIFICATE_CHAIN"))
+        privateKey.set(providers.environmentVariable("PRIVATE_KEY"))
+        password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
+    }*/
 }
